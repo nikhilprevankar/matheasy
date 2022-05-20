6c169afa18ccf6e4205fef943cb2fe1849f2783d
nikhilprevankar <nikhilprevankaroffical@gmail.com>
2022-05-20 23:24:00 +0530

    Added Modal Feature
    Added code updates for Added Modal Feature

--- src/pages/landing/HomePage.jsx
+++ src/pages/landing/HomePage.jsx
@@ -3,31 +3,37 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import AlertMe from "../../components/AlertMe";
+import ModalMe from "../../components/ModalMe";
 import NavBar from "../../components/NavBar";
 import TableMe from "../../components/TableMe";
 
 function HomePage() {
   const [grossTotal, setGrossTotal] = useState(0);
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <Container>
       <Row>
         <Col sm={12}>
-          <NavBar />
+          <NavBar total={grossTotal} showModalFn={setShowModal} />
         </Col>
       </Row>
-      <Row>
-        <Col sm={12}>
-          <AlertMe total={grossTotal} />
-        </Col>
-      </Row>
+      {showModal ? (
+        <Row>
+          <Col sm={12}>
+            <ModalMe grossTotal={grossTotal} showModalFn={setShowModal} />
+          </Col>
+        </Row>
+      ) : (
+        <></>
+      )}
       <Row>
         <Col sm={12}>
           <TableMe updateGrossTotalFn={(total) => setGrossTotal(total)} />
         </Col>
       </Row>
       <Col sm={12}>
-        <AlertMe total={grossTotal} />
+        <AlertMe total={grossTotal} showModalFn={setShowModal} />
       </Col>
     </Container>
   );
